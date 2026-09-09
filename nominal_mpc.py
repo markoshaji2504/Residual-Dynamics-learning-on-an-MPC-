9be7303db1a847d639b93404cad3ade162b8f5ff
Markos Hajivassiliou <hajivassiliou.marcos@warwick.ac.uk>
2026-09-09 17:44:34 +0300

    nominal_mpc.py

--- nominal_mpc.py
+++ nominal_mpc.py
@@ -18,69 +18,72 @@ x_next = F(ca.vertcat(theta, theta_dot), u)
 model.set_rhs('theta', x_next[0])
 model.set_rhs('theta_dot', x_next[1])
 model.setup()
+#torque maximum contraint
+u_max=5
+if __name__=="__main__":
+        
+    #creating the controller 
+    mpc = do_mpc.controller.MPC(model)
 
-#creating the controller 
-mpc = do_mpc.controller.MPC(model)
+    #setting basic parameters 
+    #mpc with no uncetianty 
+    setup_mpc = {
+        'n_horizon': 20,        # how many steps ahead the MPC plans
+        't_step': 0.02,          # must match the dt  used in RK4 discretization
+        'n_robust': 0,           # 0 = no robustness yet 
+        'store_full_solution': True,
+    }
 
-#setting basic parameters 
-#mpc with no uncetianty 
-setup_mpc = {
-    'n_horizon': 20,        # how many steps ahead the MPC plans
-    't_step': 0.02,          # must match the dt  used in RK4 discretization
-    'n_robust': 0,           # 0 = no robustness yet 
-    'store_full_solution': True,
-}
+    mpc.set_param(**setup_mpc)
 
-mpc.set_param(**setup_mpc)
+    #setting up cost function 
 
-#setting up cost function 
+    lterm = theta**2 + theta_dot**2 + 0.1 * u**2
+    mterm = theta**2 + theta_dot**2
 
-lterm = theta**2 + theta_dot**2 + 0.1 * u**2
-mterm = theta**2 + theta_dot**2
+    #registering them with the MPC object 
 
-#registering them with the MPC object 
+    mpc.set_objective(mterm=mterm, lterm=lterm)
+    mpc.set_rterm(u=0.01)
 
-mpc.set_objective(mterm=mterm, lterm=lterm)
-mpc.set_rterm(u=0.01)
+    #setting up torque constraints for the motor 
+    mpc.bounds['lower', '_u', 'u'] = -u_max
+    mpc.bounds['upper', '_u', 'u'] = u_max
 
-#setting up torque constraints for the motor 
-mpc.bounds['lower', '_u', 'u'] = -5
-mpc.bounds['upper', '_u', 'u'] = 5
+    #finalize mpc setup 
+    mpc.setup()
 
-#finalize mpc setup 
-mpc.setup()
+    #simulate 
+    simulator = do_mpc.simulator.Simulator(model)
+    simulator.set_param(t_step=0.02)
+    simulator.setup()
 
-#simulate 
-simulator = do_mpc.simulator.Simulator(model)
-simulator.set_param(t_step=0.02)
-simulator.setup()
+    #closed loop test loop 
+    x0 = np.array([0.6, 0.2]) # starting angle and angular velocity
+    mpc.x0 = x0
+    simulator.x0 = x0
+    mpc.set_initial_guess()
 
-#closed loop test loop 
-x0 = np.array([0.6, 0.2]) # starting angle and angular velocity
-mpc.x0 = x0
-simulator.x0 = x0
-mpc.set_initial_guess()
-
-for k in range(300): #determines how long the model will run for 
-    u0 = mpc.make_step(x0)
-    x0 = simulator.make_step(u0)
+    for k in range(300): #determines how long the model will run for 
+        u0 = mpc.make_step(x0)
+        x0 = simulator.make_step(u0)
 
 
-#plotting the response seeing if everything works
-mpc_graphics = do_mpc.graphics.Graphics(mpc.data)
-sim_graphics = do_mpc.graphics.Graphics(simulator.data)
-#creating figure and axis 
-fig, ax = plt.subplots(3, sharex=True, figsize=(8, 6))
-#matching plots with setup 
-mpc_graphics.add_line(var_type='_x', var_name='theta', axis=ax[0])
-mpc_graphics.add_line(var_type='_x', var_name='theta_dot', axis=ax[1])
-mpc_graphics.add_line(var_type='_u', var_name='u', axis=ax[2])
+    #plotting the response seeing if everything works
+    mpc_graphics = do_mpc.graphics.Graphics(mpc.data)
+    sim_graphics = do_mpc.graphics.Graphics(simulator.data)
+    #creating figure and axis 
+    fig, ax = plt.subplots(3, sharex=True, figsize=(8, 6))
+    #matching plots with setup 
+    mpc_graphics.add_line(var_type='_x', var_name='theta', axis=ax[0])
+    mpc_graphics.add_line(var_type='_x', var_name='theta_dot', axis=ax[1])
+    mpc_graphics.add_line(var_type='_u', var_name='u', axis=ax[2])
 
-ax[0].set_ylabel('theta [rad]')
-ax[1].set_ylabel('theta_dot [rad/s]')
-ax[2].set_ylabel('u [N*m]')
-ax[2].set_xlabel('time [s]')
+    ax[0].set_ylabel('theta [rad]')
+    ax[1].set_ylabel('theta_dot [rad/s]')
+    ax[2].set_ylabel('u [N*m]')
+    ax[2].set_xlabel('time [s]')
 
-mpc_graphics.plot_results()
-mpc_graphics.reset_axes()
-plt.show()
+    mpc_graphics.plot_results()
+    mpc_graphics.reset_axes()
+    plt.show()
