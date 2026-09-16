d200028e0d9c8e1b40255cf1d99fb68a74b72463
Markos Hajivassiliou <hajivassiliou.marcos@warwick.ac.uk>
2026-09-16 16:18:07 +0300

    generate near equilibrium data set for computing W bound

--- terminal_ingredients.py
+++ terminal_ingredients.py
@@ -2,6 +2,8 @@ from Dynamics import build_dynamics_functions
 from scipy.linalg import solve_discrete_are
 import casadi as ca
 import numpy as np
+from Data_generation import generate_dataset # function needed to generate the dataset for calculating
+#the polytope W
 
 L_TRUE = 0.5
 discrepancy_levels = [0.05, 0.15, 0.30, 0.50, 0.75]
@@ -37,3 +39,24 @@ for level in discrepancy_levels:
     }
 
     print(f"Discrepancy {level}: stable = {is_stable}, eigenvalues = {eigenvalues}")
+
+#calculate the polytope W
+#we restrict the range that the polytope is calculated for to be under 0.6 rad to better reflect
+#the range of values that the controller will encounter in the actual case 
+# this is to geneerate new data for the creation of the polytope 
+near_eq_conditions = [ 
+    [0.1, 0.0], [0.2, 0.0], [0.3, 0.0], [-0.1, 0.0], [-0.2, 0.0], [-0.3, 0.0],
+    [0.1, 0.2], [0.1, -0.2], [-0.1, 0.2], [-0.1, -0.2],
+    [0.2, 0.3], [-0.2, -0.3],
+]
+
+near_eq_data = generate_dataset(near_eq_conditions, n_steps=50, u_max=2)
+
+near_eq_states = np.array([entry[0] for entry in near_eq_data])
+near_eq_inputs = np.array([entry[1] for entry in near_eq_data])
+near_eq_next_states = np.array([entry[2] for entry in near_eq_data])
+
+print(near_eq_states.shape)
+print(near_eq_inputs.shape)
+print(near_eq_next_states.shape)
+
